1bdbbf1aade0b0fc7378239227f96449ad1e85ca
livebits <ahmad.khalilfar@gmail.com>
2024-02-04 22:02:40 +0330

    add 404 page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/ContactUs";
+import NotFound from "../pages/404";
 
 const AppRoutes = () => {
 
@@ -10,6 +11,7 @@ const AppRoutes = () => {
             <Route path="/" Component={Home} />
             <Route path="/about" Component={About} />
             <Route path="/contact" Component={Contact} />
+            <Route path="*" Component={NotFound} />
         </Routes>
     )
 }
